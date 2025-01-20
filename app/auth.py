1b2d7fa9440dd938327e5e168ea6ca52697da0f9
sanny suarez <sannysuarez4@gmail.com>
2025-01-20 13:36:31 +0100

    on it

--- app/auth.py
+++ app/auth.py
@@ -8,19 +8,26 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        name = request.form['name']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        tel = request.form['tel']
+        state = request.form['state']
+        gender = request.form['gender']
         password = request.form['password']
         db = get_db()
         error = None
 
-        if not name:
-            error = 'Name required!'
+        if not firstname:
+            error = 'First Name required!'
+        elif not email:
+            error = 'Email is required!'
         elif not password:
             error = 'password is required!'
 
         if error is None:
             try:
-                db.execute("INSERT INTO user (name, password) VALUES (?, ?)", (name, generate_password_hash(password)),)
+                db.execute("INSERT INTO user (firstname, lastname, email, tel, state, gender, password) VALUES (?, ?, ?, ?, ?, ?, ?)", (firstname, lastname, email, tel, state, gender, generate_password_hash(password)),)
                 db.commit()
             except db.IntegrityError:
                 error = f"{email} is already registered."
@@ -32,23 +39,26 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        # Used get() to safely handle missing keys.
+        email = request.form.get('email')
+        password = request.form.get('password')
         db = get_db()
         error = None
         user = db.execute('SELECT * FROM user WHERE email = ?', (email,)).fetchone()
-
+        # Check if user exists and password is valid
         if email is None:
-            error = 'Incorrect Email!'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password!'
+            error = 'Invalid Email or Password!' # General error for security reasons
+        elif not check_password_hash(user['password'], password): # Verify hashed password
+            error = 'Invalid Email or Password!' # Same error to avoid giving clues.
 
         if error is None:
+            # clear session and log in user
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
-        flash(error)
-    return render_template('auth/login.html')
+        # pass the error to flash for the user
+        flash(error, "error")
+    return render_template('auth/login.html', flash=flash)
 
 @bp.before_app_request
 def load_logged_in_user():
